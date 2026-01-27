a152a31fd56cb20d67d604ab04cc251998868e73
Uru Aki <uruaki@tsuki-2.local>
2026-01-27 12:02:33 +0900

    update

--- threading/thread_safe_iostream.cpp
+++ threading/thread_safe_iostream.cpp
@@ -6,60 +6,20 @@
 /*   By: hshimizu <hshimizu@42tokyo.student.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 15:30:58 by hshimizu          #+#    #+#             */
-/*   Updated: 2026/01/22 19:38:37 by hshimizu         ###   ########.fr       */
+/*   Updated: 2026/01/27 10:36:05 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "thread_safe_iostream.hpp"
-#include <iostream>
 
-std::mutex ThreadSafeBuf::_mtx;
-thread_local std::unique_lock<std::mutex>
-  ThreadSafeBuf::_lk(ThreadSafeBuf::_mtx, std::defer_lock);
-thread_local std::string ThreadSafeBuf::_prefix;
+template class BasicThreadSafeStreamBuf<char>;
+template class BasicThreadSafeStreamBuf<wchar_t>;
 
+template class BasicThreadSafeOStream<char>;
+template class BasicThreadSafeOStream<wchar_t>;
 
-ThreadSafeBuf::int_type ThreadSafeBuf::overflow(int_type ch) {
-  if (traits_type::eq_int_type(ch, traits_type::eof()))
-    return traits_type::not_eof(ch);
-  try {
-    if (!_lk.owns_lock()) {
-      _lk.lock();
-      std::cout.write(_prefix.c_str(), _prefix.size());
-    }
-    std::cout.put(ch);
-    if (ch == '\n') {
-      _lk.unlock();
-    }
-  } catch (...) {
-    if (_lk.owns_lock())
-      _lk.unlock();
-    throw;
-  }
-  return traits_type::not_eof(ch);
-}
+static thread_local ThreadSafeStreamBuf g_tsbuf(std::cout.rdbuf());
+thread_local ThreadSafeOStream threadSafeCout(&g_tsbuf);
 
-std::streamsize ThreadSafeBuf::xsputn(char const *s, std::streamsize n) {
-  std::streamsize written = 0;
-  for (std::streamsize i = 0; i < n; ++i) {
-    if (overflow(s[i]) == traits_type::eof())
-      break;
-    ++written;
-  }
-  return written;
-}
-
-void ThreadSafeBuf::setPrefix(std::string const prefix) {
-  _prefix = prefix;
-}
-
-void ThreadSafeCout::setPrefix(std::string const prefix) {
-  static_cast<ThreadSafeBuf *>(rdbuf())->setPrefix(prefix);
-}
-
-ThreadSafeCout::ThreadSafeCout(ThreadSafeBuf *buf)
-  : std::ostream(buf) {
-}
-
-static ThreadSafeBuf g_tsbuf;
-ThreadSafeCout threadSafeCout(&g_tsbuf);
+static thread_local ThreadSafeWStreamBuf g_tswbuf(std::wcout.rdbuf());
+thread_local ThreadSafeWOStream threadSafeWCout(&g_tswbuf);
--- threading/thread_safe_iostream.hpp
+++ threading/thread_safe_iostream.hpp
@@ -6,53 +6,209 @@
 /*   By: hshimizu <hshimizu@42tokyo.student.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 14:42:43 by hshimizu          #+#    #+#             */
-/*   Updated: 2026/01/22 19:47:46 by hshimizu         ###   ########.fr       */
+/*   Updated: 2026/01/27 12:02:08 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#include <streambuf>
-#include <sstream>
+#include <memory>
 #include <iostream>
+#include <sstream>
 #include <mutex>
+#include <unordered_map>
 #include <string>
 
-class ThreadSafeBuf : public std::streambuf {
+template <typename CharT, typename Traits = std::char_traits<CharT>>
+class BasicThreadSafeStreamBuf : public std::basic_streambuf<CharT, Traits> {
 public:
-  int_type overflow(int_type ch);
-  std::streamsize xsputn(char const *s, std::streamsize n);
-  void setPrefix(std::string const prefix);
+  typedef CharT char_type;
+  typedef Traits traits_type;
+  typedef typename traits_type::int_type int_type;
+  typedef typename traits_type::pos_type pos_type;
+  typedef typename traits_type::off_type off_type;
+
+  BasicThreadSafeStreamBuf(std::basic_streambuf<CharT, Traits> *dest);
+  ~BasicThreadSafeStreamBuf();
+
+  void setPrefix(std::basic_string<CharT, Traits> const &prefix);
+  void resetLine();
+
+protected:
+  virtual int_type overflow(int_type ch = traits_type::eof());
+  virtual std::streamsize xsputn(char_type const *s, std::streamsize n);
+  virtual int sync();
 
 private:
-  static std::mutex _mtx;
-  static thread_local std::unique_lock<std::mutex> _lk;
-  static thread_local std::string _prefix;
+  std::basic_streambuf<CharT, Traits> *_dest;
+  std::basic_string<CharT, Traits> _prefix;
+  std::unique_lock<std::mutex> _lock;
+  std::shared_ptr<std::mutex> _mtx; 
+  
+  using StreambufMutexMap
+    = std::unordered_map<std::basic_streambuf<CharT, Traits> *, std::weak_ptr<std::mutex>>;
+  static StreambufMutexMap _map;
+  static std::mutex _mapMtx;
 };
 
-class ThreadSafeCout : public std::ostream {
+using ThreadSafeStreamBuf = BasicThreadSafeStreamBuf<char>;
+using ThreadSafeWStreamBuf = BasicThreadSafeStreamBuf<wchar_t>;
+
+extern template class BasicThreadSafeStreamBuf<char>;
+extern template class BasicThreadSafeStreamBuf<wchar_t>;
+
+template <typename CharT, typename Traits = std::char_traits<CharT>>
+class BasicThreadSafeOStream : public std::basic_ostream<CharT, Traits> {
 public:
-  ThreadSafeCout(ThreadSafeBuf *buf);
-  void setPrefix(std::string const prefix);
+  typedef CharT char_type;
+  typedef Traits traits_type;
+  typedef typename traits_type::int_type int_type;
+  typedef typename traits_type::pos_type pos_type;
+  typedef typename traits_type::off_type off_type;
+
+  explicit BasicThreadSafeOStream(BasicThreadSafeStreamBuf<CharT, Traits> *buf);
+
+  void setPrefix(std::basic_string<CharT, Traits> const &prefix);
+  void resetLine();
+
+private:
+  using std::basic_ostream<CharT, Traits>::rdbuf;
 };
 
-extern ThreadSafeCout threadSafeCout;
+using ThreadSafeOStream = BasicThreadSafeOStream<char>;
+using ThreadSafeWOStream = BasicThreadSafeOStream<wchar_t>;
+
+extern template class BasicThreadSafeOStream<char>;
+extern template class BasicThreadSafeOStream<wchar_t>;
+
+extern thread_local ThreadSafeOStream threadSafeCout;
+extern thread_local ThreadSafeWOStream threadWSafeCout;
+
+template <typename CharT, typename Traits>
+BasicThreadSafeStreamBuf<CharT, Traits>::StreambufMutexMap
+BasicThreadSafeStreamBuf<CharT, Traits>::_map;
+
+template <typename CharT, typename Traits>
+std::mutex BasicThreadSafeStreamBuf<CharT, Traits>::_mapMtx;
+
+template <typename CharT, typename Traits>
+BasicThreadSafeStreamBuf<CharT, Traits>::BasicThreadSafeStreamBuf(
+  std::basic_streambuf<CharT, Traits> *dest)
+  : _dest(dest) {
+  std::lock_guard<std::mutex> guard(_mapMtx);
+  auto it = _map.find(_dest);
+  if (it != _map.end())
+    _mtx = it->second.lock();
+  else
+    _map[_dest] = _mtx = std::make_shared<std::mutex>();
+  _lock = std::unique_lock<std::mutex>(*_mtx, std::defer_lock);
+}
+
+template <typename CharT, typename Traits>
+BasicThreadSafeStreamBuf<CharT, Traits>::~BasicThreadSafeStreamBuf() {
+  if (_lock.owns_lock())
+    _lock.unlock();
+  std::lock_guard<std::mutex> guard(_mapMtx);
+  auto it = _map.find(_dest);
+  _mtx.reset();
+  if (it != _map.end() && it->second.expired())
+    _map.erase(it);
+}
+
+template <typename CharT, typename Traits>
+void BasicThreadSafeStreamBuf<CharT, Traits>::setPrefix(
+  std::basic_string<CharT, Traits> const &prefix) {
+  _prefix = prefix;
+}
+
+template <typename CharT, typename Traits>
+void BasicThreadSafeStreamBuf<CharT, Traits>::resetLine() {
+  if (_lock.owns_lock())
+    _lock.unlock();
+}
+
+template <typename CharT, typename Traits>
+BasicThreadSafeStreamBuf<CharT, Traits>::int_type
+BasicThreadSafeStreamBuf<CharT, Traits>::overflow(int_type ch) {
+  if (traits_type::eq_int_type(ch, traits_type::eof()))
+    return traits_type::not_eof(ch);
+  if (!_lock.owns_lock()) {
+    _lock.lock();
+    _dest->sputn(_prefix.data(), _prefix.size());
+  }
+  _dest->sputc(traits_type::to_char_type(ch));
+  if (traits_type::eq(traits_type::to_char_type(ch), char_type('\n')))
+    _lock.unlock();
+  return traits_type::not_eof(ch);
+}
+
+template <typename CharT, typename Traits>
+std::streamsize BasicThreadSafeStreamBuf<CharT, Traits>::xsputn(
+  char_type const *s, std::streamsize n) {
+  std::streamsize total = 0;
+  while (n > 0) {
+    char_type const*nl = std::find(s, s + n, char_type('\n'));
+    if (nl == s + n) {
+      if (n) {
+        if (!_lock.owns_lock()) {
+          _lock.lock();
+          _dest->sputn(_prefix.data(), _prefix.size());
+        }
+        _dest->sputn(s, n);
+        total += n;
+      }
+      break;
+    }
+    std::streamsize chunk = (nl - s) + 1;
+    if (!_lock.owns_lock()) {
+        _lock.lock();
+        _dest->sputn(_prefix.data(), _prefix.size());
+    }
+    _dest->sputn(s, chunk);
+    if (_lock.owns_lock())
+      _lock.unlock();
+    s += chunk;
+    n -= chunk;
+    total += chunk;
+  }
+  return total;
+}
+
+template <typename CharT, typename Traits>
+int BasicThreadSafeStreamBuf<CharT, Traits>::sync() {
+  return _dest->pubsync();
+}
+
+template <typename CharT, typename Traits>
+BasicThreadSafeOStream<CharT, Traits>::BasicThreadSafeOStream(
+  BasicThreadSafeStreamBuf<CharT, Traits> *buf)
+  : std::basic_ostream<CharT, Traits>(buf) {
+}
+
+template <typename CharT, typename Traits>
+void BasicThreadSafeOStream<CharT, Traits>::setPrefix(
+  std::basic_string<CharT, Traits> const &prefix) {
+  static_cast<BasicThreadSafeStreamBuf<CharT, Traits> *>(rdbuf())->setPrefix(prefix);
+}
+
+template <typename CharT, typename Traits>
+void BasicThreadSafeOStream<CharT, Traits>::resetLine() {
+  static_cast<BasicThreadSafeStreamBuf<CharT, Traits> *>(rdbuf())->resetLine();
+}
 
 template<typename T>
 void prompt(std::string const &question, T &dest) {
-  static std::mutex mtx;
-  std::lock_guard<std::mutex> lock(mtx);
   std::string tmp;
   for (;;) {
     threadSafeCout << question;
     std::string tmp;
     std::getline(std::cin, tmp);
+    threadSafeCout.resetLine();
     if (!std::cin)
       throw std::runtime_error("input stream error");
     std::istringstream iss(tmp);
     iss >> dest;
     if (iss)
       break;
-    threadSafeCout << "\033[F" << std::endl;
   }
 }
