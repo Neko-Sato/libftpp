5c9b412e9d78471a4b34389830fd0f98d336a577
Uru Aki <uruaki@tsuki-2.local>
2026-01-31 23:32:27 +0900

    updat

--- threading/threading.hpp
+++ threading/threading.hpp
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@42tokyo.student.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/28 11:17:27 by hshimizu          #+#    #+#             */
-/*   Updated: 2026/01/28 11:19:26 by hshimizu         ###   ########.fr       */
+/*   Updated: 2026/01/31 23:31:59 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,4 @@
 #include "thread_safe_iostream.hpp"
 #include "thread_safe_queue.hpp"
 #include "worker_pool.hpp"
+#include "persistent_worker.hpp"
