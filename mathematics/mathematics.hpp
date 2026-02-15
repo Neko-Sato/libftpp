7ae05c9a100b84758d49a876148931328408c7a6
Uru Aki <uruaki@tsuki-2.local>
2026-02-15 19:18:49 +0900

    update

---
+++ mathematics/mathematics.hpp
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mathematics.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hshimizu <hshimizu@42tokyo.student.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/15 19:16:21 by hshimizu          #+#    #+#             */
+/*   Updated: 2026/02/15 19:17:11 by hshimizu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "ivector2.hpp"
+#include "ivector3.hpp"
+#include "perlin_noise_2D.hpp"
+#include "random_2D_coordinate_generator.hpp"
